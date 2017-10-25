bee3bc452b62aa00d1190a8fa57bbff37c599700
Aleksandar Manev <100586500@student.swin.edu.au>
2017-10-25 18:36:50 +1100

    h

--- web.js
+++ web.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use("/app", express.static(path.join(__dirname, '../index.html')));
+
 app.get('/', function (req, res) {
   res.send('hello, world');
 });
